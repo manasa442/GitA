ea2606defdabfa4592875826f71c8de930133fbc
unknown <442manasa@gmail.com>
2020-12-14 19:28:02 +0530

    First commit

---
+++ src/test/java/qaclickacademy/Mavenjava1/RESTAPITest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.Mavenjava1;
+
+import org.testng.annotations.Test;
+public class RESTAPITest {
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira1");
+		System.out.println("posJira1");
+		System.out.println("posJira1");
+
+
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+
+}
